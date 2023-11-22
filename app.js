98e00a492da5687a80eaa9072806cee7ce67ee0d
Syed Sami-Ul-Haider <sambukhari465@gmail.com>
2023-11-22 13:21:31 +0500

    express crud

--- app.js
+++ app.js
@@ -1,17 +1,40 @@
 const express = require("express");
-const app = express();
 const multer = require("multer");
-const jwt = require("jsonwebtoken");
-const {createUser, deleteUser, getOneUser, updateUser, getAllUser} = require('./controller/user')
+const path = require("path")
+const app = express();
+const {
+  createUser,
+  deleteUser,
+  getOneUser,
+  updateUser,
+  getAllUser,
+  loginUser,
+  verifyToken,
+} = require("./controller/user");
 
 app.use(express.json());
 
-app.post('/create', createUser)
-app.get('/', getAllUser)
-app.get('/get/:id', getOneUser)
-app.delete('/delete/:id', deleteUser)
-app.put('/update/:id', updateUser)
-
-app.listen(4005, ()=>{
-    console.log(`Server run on port ${4005}`)
+// ===========================Multer Use for Image upload================================
+app.use('/', express.static(path.join(__dirname , 'images')))
+const storage = multer.diskStorage({
+  destination:function (req,file,cb) {
+    cb(null, 'images')
+  },
+  filename: function(req,file,cb){
+    cb(null, file.originalname)
+  }
 })
+ 
+const upload = multer({storage})
+
+// ================================All Routers==================================
+app.post("/create", upload.single("image"), createUser);
+app.get("/", verifyToken, getAllUser);
+app.get("/get/:id", getOneUser);
+app.delete("/delete/:id", deleteUser)
+app.put("/update/:id", updateUser);
+app.post("/login", loginUser);
+
+app.listen(4005, () => {
+  console.log(`Server run on port ${4005}`);
+});
--- controller/user.js
+++ controller/user.js
@@ -1,18 +1,30 @@
 const jwt = require("jsonwebtoken");
+// ================================Create array of Objects==================================
 const data = [
-  { id: 1, name: "younas", age: "24", email: "younas@gmail.com" },
-  { id: 2, name: "sami", age: "24", email: "sami@gmail.com" },
-  { id: 3, name: "talha", age: "24", email: "talha@gmail.com" },
+  {
+    id: 1,
+    name: "younas",
+    age: "24",
+    email: "younas@gmail.com",
+    password: 12345,
+  },
+  { id: 2, name: "sami", age: "24", email: "sami@gmail.com", password: 12345 },
+  {
+    id: 3,
+    name: "talha",
+    age: "24",
+    email: "talha@gmail.com",
+    password: 12345,
+  },
 ];
+
+// ===============================User Create and Existing Part===========================
 exports.createUser = (req, res) => {
   try {
-    const { id } = (req.body);
+    const { id } = req.body;
     const user = data.find((item) => item.id === id);
     if (user) {
-      // return res.send(user);
-        //  return res.send("user allready exist")
-        console.log("User All Ready Exists")
-        return res.json({ error: "User already exists" });
+      return res.send("user allready exist");
     } else {
       data.push(req.body);
       return res.send(data);
@@ -22,18 +34,24 @@ exports.createUser = (req, res) => {
     return res.json({ error: "Internal server error" });
   }
 };
+// ====================================Get All User Data=====================================
 exports.getAllUser = (req, res) => {
-    return res.send(data);
-  };
+  return res.send(data);
+};
+
+// =============================Get One User Data with Params=================================
 exports.getOneUser = (req, res) => {
   const findUser = data.find((item) => item.id === parseInt(req.params.id));
   return res.send(findUser);
 };
+
+// ==============================Delete User Data With Params==================================
 exports.deleteUser = (req, res) => {
   const { id } = req.params;
   const newData = data.filter((item) => item.id !== parseInt(id));
   return res.send(newData);
 };
+// ==============================Updata User Data With Params===================================
 exports.updateUser = (req, res) => {
   const { id, name, age, email } = req.body;
   const findUser = data.find((item) => item.id === parseInt(req.params.id));
@@ -50,11 +68,29 @@ exports.updateUser = (req, res) => {
     if (email) {
       findUser.email = email;
     }
-    // findUser.id = id;
-    // findUser.name = name;
-    // findUser.age = age;
-    // findUser.email = email
   }
-  // data.save(findUser)
+
   res.send(data);
+};
+
+// ====================================JWT token Genrate=====================================
+exports.loginUser = (req, res) => {
+  const { email, password } = req.body;
+  const newData = data.find(
+    (item) => item.email === email && item.password === password
+  );
+  if (!newData) {
+    return res.send("not match");
+  }
+  const token = jwt.sign({ email }, "secretkey");
+  res.json({ token });
+};
+
+// ====================================Jwt token Verify=====================================
+exports.verifyToken = (req, res, next) => {
+  const token = req.headers.token;
+  const decode = jwt.verify(token, "secretkey");
+  if(decode){
+    next()
+  }
 };
